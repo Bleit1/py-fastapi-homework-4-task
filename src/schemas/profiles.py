4ad855c51760fe3321654cbd2c4719940d5891a6
Arsenii Aristov <arseniyaristov07@gmail.com>
2025-03-18 11:46:46 +0200

    Solution

--- src/schemas/profiles.py
+++ src/schemas/profiles.py
@@ -1,13 +1,20 @@
 from datetime import date
 
-from fastapi import UploadFile, Form, File, HTTPException
-from pydantic import BaseModel, field_validator, HttpUrl
+from pydantic import BaseModel, field_serializer
 
-from validation import (
-    validate_name,
-    validate_image,
-    validate_gender,
-    validate_birth_date
-)
+from database.models.accounts import GenderEnum
 
-# Write your code here
+
+class ProfileResponseSchema(BaseModel):
+    id: int
+    user_id: int
+    first_name: str
+    last_name: str
+    gender: GenderEnum
+    date_of_birth: date
+    info: str
+    avatar: str
+
+    @field_serializer("first_name", "last_name")
+    def serialize_lower(self, value: str) -> str:
+        return value.lower()
